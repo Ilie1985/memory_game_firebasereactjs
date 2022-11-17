d37fd710b3a0ee7d533e3d7cc48dffc650856167
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-17 16:45:47 +0000

    use disable as a prop

--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ card,handleChoice,flipped }) => {
-
-const handleClick=()=>{
-handleChoice(card)
-}
-
-
-
+const Card = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
 
   return (
-    <div className="card" >
-              <div className={flipped ? "flipped" : ""}>
-                <img src={card.src} alt="front of the card" className="front" />
-                <img
-                  src="/img/cover.png"
-                  alt="back of the card"
-                  className="back"
-                  onClick={handleClick}
-                />
-              </div>
-            </div>
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="front of the card" className="front" />
+        <img
+          src="/img/cover.png"
+          alt="back of the card"
+          className="back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
   );
 };
 
